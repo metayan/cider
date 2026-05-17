baef452d0abc23e1195d3be5231e1fb59d402aa4
Yan <yan@metatem.net>
2026-05-17 00:45:41 +0000

    Make removal of *cider-result* buffer after an error optional
    The *cider-result* buffer is normally removed after an error.
    When it is pops up again, it can be in a different window.
    
    By adding the new option cider-popup-eval-error-handler-function
    the removal is made optional, so the *cider-result* can stay in the
    same place without popping around to different windows.
    
    The current behaviour of removing the buffer upon error has been kept
    by adding the function cider-remove-popup-buffer and setting the new
    option to call it.
    
    If cider-popup-eval-error-handler-function is set to nil,
    the *cider-result* buffer is not removed after an error.

--- lisp/cider-eval.el
+++ lisp/cider-eval.el
@@ -754,6 +754,20 @@ COMMENT-POSTFIX is the text to output after the last line."
                      comment-prefix continued-prefix comment-postfix)))
                 (cider--maybe-set-eval-register res)))))
 
+(defun cider-remove-popup-buffer (chosen-buffer)
+  "Remove popup buffer"
+  (when (and (buffer-live-p chosen-buffer)
+             (member (buffer-name chosen-buffer)
+                     cider-ancillary-buffers))
+    (with-selected-window (get-buffer-window chosen-buffer)
+      (cider-popup-buffer-quit-function t))))
+
+(defvar cider-popup-eval-error-handler-function
+  (lambda (chosen-buffer)
+    (cider-remove-popup-buffer chosen-buffer))
+  "What to do after an error when evaluating to cider-result-buffer.
+nil keeps the buffer.")
+
 (defun cider-popup-eval-handler (&optional buffer _bounds source-buffer)
   "Make a handler for printing evaluation results in popup BUFFER,
 _BOUNDS representing the buffer bounds of the evaled input,
@@ -770,11 +784,8 @@ This is used by pretty-printing commands."
      :on-stdout #'cider-emit-interactive-eval-output
      :on-stderr #'cider-emit-interactive-eval-err-output
      :on-eval-error (lambda ()
-                      (when (and (buffer-live-p chosen-buffer)
-                                 (member (buffer-name chosen-buffer)
-                                         cider-ancillary-buffers))
-                        (with-selected-window (get-buffer-window chosen-buffer)
-                          (cider-popup-buffer-quit-function t)))
+                      (when cider-popup-eval-error-handler-function
+                        (funcall cider-popup-eval-error-handler-function chosen-buffer))
                       ;; also call the default nrepl-err-handler-function so
                       ;; our custom behavior doesn't void the base behavior:
                       (when nrepl-err-handler-function
